bde31151fa51edb69733c318f24e203e7001368e
DaOnlySonic <31445804+DaOnlySonic@users.noreply.github.com>
2017-08-30 22:41:41 +0100

    Update main.php

--- src/Nick/main.php
+++ src/Nick/main.php
@@ -5,4 +5,7 @@ namespace sonic\nick;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\player;
 use pocketmine\event\listener;
-//this aint done
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginBase;
+//This is NOT finished!

abb582701c908a69dd700b1e0dd02b86733acd67
DaOnlySonic <31445804+DaOnlySonic@users.noreply.github.com>
2017-09-27 22:08:21 +0100

    Update main.php

--- src/Nick/main.php
+++ src/Nick/main.php
@@ -8,6 +8,10 @@ use pocketmine\event\listener;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\server\remoteservercommandevent;
+
+if ($sender instanceof Console){
+  $sender->sendMessage(TF::RED . "This command is for players only! Please join the server and run again!");
 
 /*
 * $sender->sendMessage($this->prefix.TF::DARK_GRAY . "You are now" . TF::RED . "nicked!");
